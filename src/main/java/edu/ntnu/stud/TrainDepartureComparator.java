4b2bcdc097ea61c2ed761aa48008cb5a58f52c64
servan <servank@stud.ntnu.no>
2023-10-18 17:03:12 +0200

    Documented TrainDepartureComparator.java Javadoc style

--- src/main/java/edu/ntnu/stud/TrainDepartureComparator.java
+++ src/main/java/edu/ntnu/stud/TrainDepartureComparator.java
@@ -1,11 +1,20 @@
 package edu.ntnu.stud;
-
-import edu.ntnu.stud.TrainDeparture;
-
 import java.util.Comparator;
 
-import java.util.Comparator;
+/**
+ * The TrainDepartureComparator class is a custom comparator used to compare TrainDeparture objects
+ * based on their departure times.
+ */
 public class TrainDepartureComparator implements Comparator<TrainDeparture> {
+
+    /**
+     * Compares two TrainDeparture objects based on their departure times.
+     *
+     * @param departure1 The first TrainDeparture object to compare.
+     * @param departure2 The second TrainDeparture object to compare.
+     * @return A negative integer if departure1 precedes departure2 in terms of departure time,
+     *         zero if they have the same departure time, or a positive integer if departure1 follows departure2.
+     */
     @Override
     public int compare(TrainDeparture departure1, TrainDeparture departure2) {
         return departure1.getDepartureTime().compareTo(departure2.getDepartureTime());
