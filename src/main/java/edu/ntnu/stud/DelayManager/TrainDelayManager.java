c5fe0e7463a3d1fc6c54cb2332d0e6a6cc42fd82
servan <servank@stud.ntnu.no>
2023-11-28 14:22:44 +0100

    Commented TrainDelayManager.java

--- src/main/java/edu/ntnu/stud/DelayManager/TrainDelayManager.java
+++ src/main/java/edu/ntnu/stud/DelayManager/TrainDelayManager.java
@@ -1,13 +1,25 @@
 package edu.ntnu.stud.DelayManager;
-
-
-
 import edu.ntnu.stud.Departures.TrainDeparture;
-
 import java.time.LocalTime;
 import java.util.List;
 
+/**
+ * The TrainDelayManager class provides functionality to manage delays of train departures.
+ * It allows adding a specified delay to a train departure identified by its train number.
+ */
+
 public class TrainDelayManager {
+
+    /**
+     * Adds a specified delay to a train departure.
+     * This method searches for a departure by its train number in the provided list of departures.
+     * If the departure is found, the specified delay is added to it.
+     *
+     * @param departures  A list of TrainDeparture objects to search through.
+     * @param trainNumber The train number of the departure to which the delay is to be added.
+     * @param delay       The delay to be added to the departure time.
+     * @throws IllegalArgumentException if no departure with the specified train number is found.
+     */
     public static void addDelay(List<TrainDeparture> departures, String trainNumber, LocalTime delay) {
         // Søk etter avgangen basert på tognummer
         TrainDeparture departureToAddDelay = null;
