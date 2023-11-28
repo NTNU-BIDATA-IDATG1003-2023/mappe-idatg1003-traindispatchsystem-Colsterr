1fc06d3133ad9ba13f3e239fabd9b86fb0c6ffbc
servan <servank@stud.ntnu.no>
2023-11-28 14:43:34 +0100

    Commented TrainDepartureRegister.java

--- src/main/java/edu/ntnu/stud/Departures/TrainDepartureRegister.java
+++ src/main/java/edu/ntnu/stud/Departures/TrainDepartureRegister.java
@@ -1,24 +1,41 @@
 package edu.ntnu.stud.Departures;
-
-import edu.ntnu.stud.Departures.TrainDeparture;
-import edu.ntnu.stud.Departures.TrainDepartureComparator;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The TrainDepartureRegister class maintains a list of all train departures.
+ * It provides functionality to add, search, and modify train departures in the list.
+ */
+
 public class TrainDepartureRegister {
     private List<TrainDeparture> departures;
+
+    /**
+     * Constructs a new TrainDepartureRegister.
+     */
 
     public TrainDepartureRegister() {
         departures = new ArrayList<>();
     }
 
+    /**
+     * Adds a new TrainDeparture to the register.
+     *
+     * @param newDeparture The TrainDeparture to be added.
+     */
+
     public void addTrainDeparture(TrainDeparture newDeparture) {
         departures.add(newDeparture);
     }
 
+    /**
+     * Finds a TrainDeparture by its train number.
+     *
+     * @param trainNumber The train number of the departure to find.
+     * @return The TrainDeparture with the specified train number, or null if not found.
+     */
     public TrainDeparture findTrainByTrainNumber(String trainNumber) {
         for (TrainDeparture departure : departures) {
             if (departure.getTrainNumber().equals(trainNumber)) {
@@ -28,11 +45,24 @@ public class TrainDepartureRegister {
         return null;
     }
 
+    /**
+     * Finds all departures to a specific destination.
+     *
+     * @param destination The destination to search for.
+     * @return A list of TrainDepartures to the specified destination.
+     */
+
     public List<TrainDeparture> findDeparturesByDestination(String destination) {
         return departures.stream()
                 .filter(departure -> departure.getDestination().equalsIgnoreCase(destination))
                 .collect(Collectors.toList());
     }
+
+    /**
+     * Removes departures from the register that are before a specified time.
+     *
+     * @param time The time to compare against for removing departures.
+     */
 
     public void removeDeparturesBeforeTime(String time) {
         Iterator<TrainDeparture> iterator = departures.iterator();
@@ -44,12 +74,23 @@ public class TrainDepartureRegister {
         }
     }
 
+    /**
+     * Gets a list of departures sorted by departure time.
+     *
+     * @return A sorted list of TrainDepartures.
+     */
+
     public List<TrainDeparture> getSortedDepartures() {
         List<TrainDeparture> sortedDepartures = new ArrayList<>(departures);
         sortedDepartures.sort(new TrainDepartureComparator());
         return sortedDepartures;
     }
 
+    /**
+     * Gets the list of all train departures.
+     *
+     * @return The list of all TrainDepartures.
+     */
     public List<TrainDeparture> getDepartures() {
         return departures;
     }
