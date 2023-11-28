c4fd24f0bc6e380cf007cc1d756a4e260805885d
servan <servank@stud.ntnu.no>
2023-11-28 14:21:26 +0100

    Commented TimeUpdate.java

--- src/main/java/edu/ntnu/stud/Clock/TimeUpdate.java
+++ src/main/java/edu/ntnu/stud/Clock/TimeUpdate.java
@@ -4,12 +4,30 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
+/**
+ * The TimeUpdate class is used for managing the current time within the system.
+ * It allows updating the system's time based on user input.
+ */
 public class TimeUpdate {
     public LocalTime currentTime;
+
+    /**
+     * Constructs a TimeUpdate object with an initial time.
+     *
+     * @param initialTime The initial time to set for the system.
+     */
 
     public TimeUpdate(LocalTime initialTime) {
         this.currentTime = initialTime;
     }
+
+    /**
+     * Updates the current time of the system based on user input.
+     * The method prompts the user to enter a new time, which is then parsed and
+     * used to update the currentTime field.
+     *
+     * @param scanner The Scanner object used for reading user input.
+     */
 
     public void updateCurrentTime(Scanner scanner) {
         System.out.println("Nåværende tidspunkt: " + currentTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
@@ -18,7 +36,6 @@ public class TimeUpdate {
         String inputTime = scanner.next();
         LocalTime newTime = LocalTime.parse(inputTime, DateTimeFormatter.ofPattern("HH:mm"));
 
-        // Oppdater tidspunktet
         currentTime = newTime;
 
         System.out.println("Tidspunktet er oppdatert til " + currentTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
